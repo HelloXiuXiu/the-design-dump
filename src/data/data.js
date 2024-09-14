c60445595c5c2c01a1d6427fb58fc7ac6b9b0361
HelloXiuXiu <xiuxiuweb@gmail.com>
2024-09-14 21:36:47 +0200

    ux laws link added

--- src/data/data.js
+++ src/data/data.js
@@ -1,470 +1,473 @@
 export const links = [
   {
-   name: 'Website galleries',
-   ref: 'galeries'
+    name: "Website galleries",
+    ref: "galeries",
   },
   {
-   name: 'Tools for images',
-   ref: 'images'
+    name: "Tools for images",
+    ref: "images",
   },
   {
-   name: 'Fonts',
-   ref: 'fonts'
+    name: "Fonts",
+    ref: "fonts",
   },
   {
-   name: 'Stocks',
-   ref: 'stocks'
+    name: "Stocks",
+    ref: "stocks",
   },
   {
-   name: 'HTML/CSS/JS',
-   ref: 'code'
-  }
-]
+    name: "HTML/CSS/JS",
+    ref: "code",
+  },
+];
 
 export const dumpGallery = [
   {
-    image: 'image-48.jpg',
-    ref: 'https://roundtwoagency.com/'
+    image: "image-48.jpg",
+    ref: "https://roundtwoagency.com/",
   },
   {
-    image: 'image-47.jpg',
-    ref: 'https://constraint.systems/'
+    image: "image-47.jpg",
+    ref: "https://constraint.systems/",
   },
   {
-    image: 'image-46.jpg',
-    ref: 'https://www.themidnightclub.com/'
+    image: "image-46.jpg",
+    ref: "https://www.themidnightclub.com/",
   },
   {
-    image: 'image-45.jpg',
-    ref: 'https://www.bramnaus.com/'
+    image: "image-45.jpg",
+    ref: "https://www.bramnaus.com/",
   },
   {
-    image: 'image-44.jpg',
-    ref: 'https://www.jeandawson.com/'
+    image: "image-44.jpg",
+    ref: "https://www.jeandawson.com/",
   },
   {
-    image: 'image-43.jpg',
-    ref: 'https://mouthwash.studio/'
+    image: "image-43.jpg",
+    ref: "https://mouthwash.studio/",
   },
   {
-    image: 'image-42.jpg',
-    ref: 'https://research.mouthwash.studio/'
+    image: "image-42.jpg",
+    ref: "https://research.mouthwash.studio/",
   },
   {
-    image: 'image-41.jpg',
-    ref: 'https://untangld.co/'
+    image: "image-41.jpg",
+    ref: "https://untangld.co/",
   },
   {
-    image: 'image-40.jpg',
-    ref: 'https://seizon.io/'
+    image: "image-40.jpg",
+    ref: "https://seizon.io/",
   },
   {
-    image: 'image-39.jpg',
-    ref: 'https://ellipsis.zip/'
+    image: "image-39.jpg",
+    ref: "https://ellipsis.zip/",
   },
   {
-    image: 'image-38.jpeg',
-    ref: 'https://habinablast.fyi/'
+    image: "image-38.jpeg",
+    ref: "https://habinablast.fyi/",
   },
   {
-    image: 'image-37.jpeg',
-    ref: 'https://cosmos.art/'
+    image: "image-37.jpeg",
+    ref: "https://cosmos.art/",
   },
   {
-    image: 'image-36.jpeg',
-    ref: 'https://locomotive.ca/en'
+    image: "image-36.jpeg",
+    ref: "https://locomotive.ca/en",
   },
   {
-    image: 'image-35.jpeg',
-    ref: 'https://postevand.com/'
+    image: "image-35.jpeg",
+    ref: "https://postevand.com/",
   },
   {
-    image: 'image-34.jpeg',
-    ref: 'https://arvinleeuwis.xyz/'
+    image: "image-34.jpeg",
+    ref: "https://arvinleeuwis.xyz/",
   },
   {
-    image: 'image-33.jpeg',
-    ref: 'https://f-9-9.com/'
+    image: "image-33.jpeg",
+    ref: "https://f-9-9.com/",
   },
   {
-    image: 'image-32.jpeg',
-    ref: 'https://felixhieronimus.com/index.html'
+    image: "image-32.jpeg",
+    ref: "https://felixhieronimus.com/index.html",
   },
   {
-    image: 'image-31.jpeg',
-    ref: 'https://claraschweers.com/'
+    image: "image-31.jpeg",
+    ref: "https://claraschweers.com/",
   },
   {
-    image: 'image-30.jpeg',
-    ref: 'https://www.yproject.fr/'
+    image: "image-30.jpeg",
+    ref: "https://www.yproject.fr/",
   },
   {
-    image: 'image-29.jpeg',
-    ref: 'https://nosajthing.com/'
+    image: "image-29.jpeg",
+    ref: "https://nosajthing.com/",
   },
   {
-    image: 'image-28.jpeg',
-    ref: 'https://wrdlss.club/'
+    image: "image-28.jpeg",
+    ref: "https://wrdlss.club/",
   },
   {
-    image: 'image-27.jpeg',
-    ref: 'https://stevnwu.com/'
+    image: "image-27.jpeg",
+    ref: "https://stevnwu.com/",
   },
   {
-    image: 'image-26.jpeg',
-    ref: 'https://2021-2041.com/'
+    image: "image-26.jpeg",
+    ref: "https://2021-2041.com/",
   },
   {
-    image: 'image-25.jpeg',
-    ref: 'https://www.theyouturn.nl/'
+    image: "image-25.jpeg",
+    ref: "https://www.theyouturn.nl/",
   },
   {
-    image: 'image-24.jpeg',
-    ref: 'https://homelesspets.su/'
+    image: "image-24.jpeg",
+    ref: "https://homelesspets.su/",
   },
   {
-    image: 'image-23.jpeg',
-    ref: 'https://xiuxiuxiuxiuxiu.com/'
+    image: "image-23.jpeg",
+    ref: "https://xiuxiuxiuxiuxiu.com/",
   },
   {
-    image: 'image-22.jpeg',
-    ref: 'https://anacuna.com/en/'
+    image: "image-22.jpeg",
+    ref: "https://anacuna.com/en/",
   },
   {
-    image: 'image-21.jpeg',
-    ref: 'https://www.redis.agency/'
+    image: "image-21.jpeg",
+    ref: "https://www.redis.agency/",
   },
   {
-    image: 'image-20.jpeg',
-    ref: 'https://atlason.com/'
+    image: "image-20.jpeg",
+    ref: "https://atlason.com/",
   },
   {
-    image: 'image-19.jpeg',
-    ref: 'https://katkow.net/'
+    image: "image-19.jpeg",
+    ref: "https://katkow.net/",
   },
   {
-    image: 'image-18.jpeg',
-    ref: 'https://regrocery.co/'
+    image: "image-18.jpeg",
+    ref: "https://regrocery.co/",
   },
   {
-    image: 'image-17.jpeg',
-    ref: 'https://studio-push.com/'
+    image: "image-17.jpeg",
+    ref: "https://studio-push.com/",
   },
   {
-    image: 'image-16.jpeg',
-    ref: 'https://bartlebooth.org/'
+    image: "image-16.jpeg",
+    ref: "https://bartlebooth.org/",
   },
   {
-    image: 'image-15.jpeg',
-    ref: 'https://xn--smon-vpa.com/'
+    image: "image-15.jpeg",
+    ref: "https://xn--smon-vpa.com/",
   },
   {
-    image: 'image-14.jpeg',
-    ref: 'https://os.foundation.app/'
+    image: "image-14.jpeg",
+    ref: "https://os.foundation.app/",
   },
   {
-    image: 'image-13.jpeg',
-    ref: 'https://www.adidas.com/us/premiumbasics-lookbook/'
+    image: "image-13.jpeg",
+    ref: "https://www.adidas.com/us/premiumbasics-lookbook/",
   },
   {
-    image: 'image-12.jpeg',
-    ref: 'https://www.emmabackman.com/'
+    image: "image-12.jpeg",
+    ref: "https://www.emmabackman.com/",
   },
   {
-    image: 'image-11.jpeg',
-    ref: 'https://verointernational.com/'
+    image: "image-11.jpeg",
+    ref: "https://verointernational.com/",
   },
   {
-    image: 'image-10.jpeg',
-    ref: 'https://janniswichmann.com/de'
+    image: "image-10.jpeg",
+    ref: "https://janniswichmann.com/de",
   },
   {
-    image: 'image-9.jpeg',
-    ref: 'https://littletroop.com/'
+    image: "image-9.jpeg",
+    ref: "https://littletroop.com/",
   },
   {
-    image: 'image-8.jpeg',
-    ref: 'https://marcd.co/'
+    image: "image-8.jpeg",
+    ref: "https://marcd.co/",
   },
   {
-    image: 'image-7.jpeg',
-    ref: 'https://okok.services/'
+    image: "image-7.jpeg",
+    ref: "https://okok.services/",
   },
   {
-    image: 'image-6.jpeg',
-    ref: 'https://curvesbyseanbrown.com/'
+    image: "image-6.jpeg",
+    ref: "https://curvesbyseanbrown.com/",
   },
   {
-    image: 'image-5.jpeg',
-    ref: 'https://samchermayeffoffice.com'
+    image: "image-5.jpeg",
+    ref: "https://samchermayeffoffice.com",
   },
   {
-    image: 'image-4.jpeg',
-    ref: 'https://areyou.okok.services/'
+    image: "image-4.jpeg",
+    ref: "https://areyou.okok.services/",
   },
   {
-    image: 'image-3.jpeg',
-    ref: 'https://cwb.fr/'
+    image: "image-3.jpeg",
+    ref: "https://cwb.fr/",
   },
   {
-    image: 'image-2.jpeg',
-    ref: 'https://radio.syg.ma/'
+    image: "image-2.jpeg",
+    ref: "https://radio.syg.ma/",
   },
   {
-    image: 'image-1.jpeg',
-    ref: 'https://ogcrystals.com/'
-  }
-]
+    image: "image-1.jpeg",
+    ref: "https://ogcrystals.com/",
+  },
+];
 
 export const codeList = [
   {
-    name: 'GSAP animation lib',
-    ref: 'https://greensock.com/'
+    name: "Laws of UX",
+    ref: "https://lawsofux.com/",
   },
   {
-    name: 'JS + CSS animation article',
-    ref: 'https://css-tricks.com/controlling-css-animations-transitions-javascript/'
+    name: "GSAP animation lib",
+    ref: "https://greensock.com/",
   },
   {
-    name: 'Css animation',
-    ref: 'https://web.dev/animations-guide/'
+    name: "JS + CSS animation article",
+    ref: "https://css-tricks.com/controlling-css-animations-transitions-javascript/",
   },
   {
-    name: 'Variable fonts animation',
-    ref: 'http://front-test.tilda.ws/variable-font-info'
+    name: "Css animation",
+    ref: "https://web.dev/animations-guide/",
   },
   {
-    name: 'p5.js',
-    ref: 'https://p5js.org/'
+    name: "Variable fonts animation",
+    ref: "http://front-test.tilda.ws/variable-font-info",
   },
   {
-    name: 'processing.org',
-    ref: 'https://processing.org/'
+    name: "p5.js",
+    ref: "https://p5js.org/",
   },
   {
-    name: 'Coding Train',
-    ref: 'https://thecodingtrain.com/'
+    name: "processing.org",
+    ref: "https://processing.org/",
   },
   {
-    name: 'BBE creative coding',
-    ref: 'https://lms.bangbangeducation.ru/subscription/23-kreativnyy-koding/'
+    name: "Coding Train",
+    ref: "https://thecodingtrain.com/",
   },
   {
-    name: '3D text generator',
-    ref: 'https://spacetypegenerator.com/'
+    name: "BBE creative coding",
+    ref: "https://lms.bangbangeducation.ru/subscription/23-kreativnyy-koding/",
   },
   {
-    name: 'Validator',
-    ref: 'https://validator.w3.org/nu/'
+    name: "3D text generator",
+    ref: "https://spacetypegenerator.com/",
   },
   {
-    name: 'Roadmap',
-    ref: 'https://roadmap.sh/frontend'
+    name: "Validator",
+    ref: "https://validator.w3.org/nu/",
   },
   {
-    name: 'W3 JS course',
-    ref: 'https://campus.w3schools.com/collections/course-catalog/products/javascript-course'
+    name: "Roadmap",
+    ref: "https://roadmap.sh/frontend",
   },
   {
-    name: 'Freecode tutorials',
-    ref: 'https://www.freecodecamp.org/learn/'
+    name: "W3 JS course",
+    ref: "https://campus.w3schools.com/collections/course-catalog/products/javascript-course",
   },
   {
-    name: 'Frontend masters',
-    ref: 'https://frontendmasters.com/learn/'
+    name: "Freecode tutorials",
+    ref: "https://www.freecodecamp.org/learn/",
   },
   {
-    name: 'Autoprefixer',
-    ref: 'https://autoprefixer.github.io/ru/'
+    name: "Frontend masters",
+    ref: "https://frontendmasters.com/learn/",
   },
   {
-    name: 'Git Fork',
-    ref: 'https://git-fork.com/'
+    name: "Autoprefixer",
+    ref: "https://autoprefixer.github.io/ru/",
   },
   {
-    name: 'Regexper',
-    ref: 'https://regexper.com/'
+    name: "Git Fork",
+    ref: "https://git-fork.com/",
   },
   {
-    name: 'Regexr',
-    ref: 'https://regexr.com/'
+    name: "Regexper",
+    ref: "https://regexper.com/",
   },
   {
-    name: 'Twitter threads',
-    ref: 'https://guides.hexlet.io/ru/usefull-twitter-threads/'
-  }
-]
+    name: "Regexr",
+    ref: "https://regexr.com/",
+  },
+  {
+    name: "Twitter threads",
+    ref: "https://guides.hexlet.io/ru/usefull-twitter-threads/",
+  },
+];
 
 export const stockList = [
   {
-    name: 'Photo stock (free)',
-    ref: 'https://www.pexels.com'
+    name: "Photo stock (free)",
+    ref: "https://www.pexels.com",
   },
   {
-    name: 'Vector and more',
-    ref: 'https://www.freepik.com/'
+    name: "Vector and more",
+    ref: "https://www.freepik.com/",
   },
   {
-    name: 'Illustrations',
-    ref: 'https://storyset.com/'
+    name: "Illustrations",
+    ref: "https://storyset.com/",
   },
   {
-    name: '3D models',
-    ref: 'https://sketchfab.com'
+    name: "3D models",
+    ref: "https://sketchfab.com",
   },
   {
-    name: 'Background videos',
-    ref: 'https://www.youtube.com/c/FreeStuffFootageTemplatesArtworks/videos'
+    name: "Background videos",
+    ref: "https://www.youtube.com/c/FreeStuffFootageTemplatesArtworks/videos",
   },
   {
-    name: 'Gifs',
-    ref: 'https://giphy.com/'
+    name: "Gifs",
+    ref: "https://giphy.com/",
   },
   {
-    name: 'Icons',
-    ref: 'https://www.flaticon.com'
-  }
-]
+    name: "Icons",
+    ref: "https://www.flaticon.com",
+  },
+];
 
 export const fontList = [
   {
-    name: 'Cargo fonts collection',
-    ref: 'https://cargo.site/Fonts'
+    name: "Cargo fonts collection",
+    ref: "https://cargo.site/Fonts",
   },
   {
-    name: 'fontshare.com (free)',
-    ref: 'https://www.fontshare.com/'
+    name: "fontshare.com (free)",
+    ref: "https://www.fontshare.com/",
   },
   {
-    name: 'type-department.com',
-    ref: 'https://type-department.com'
+    name: "type-department.com",
+    ref: "https://type-department.com",
   },
   {
-    name: 'goodtypefoundry.com',
-    ref: 'https://goodtypefoundry.com'
+    name: "goodtypefoundry.com",
+    ref: "https://goodtypefoundry.com",
   },
   {
-    name: 'type.today',
-    ref: 'https://type.today/'
+    name: "type.today",
+    ref: "https://type.today/",
   },
   {
-    name: 'https://velvetyne.fr',
-    ref: 'velvetyne.fr'
-  }
-]
+    name: "https://velvetyne.fr",
+    ref: "velvetyne.fr",
+  },
+];
 
 export const graphicList = [
   {
-    name: 'Image compressor',
-    ref: 'https://imagecompressor.com/ru/'
+    name: "Image compressor",
+    ref: "https://imagecompressor.com/ru/",
   },
   {
-    name: 'Image compressor (2)',
-    ref: 'https://compressor.io/'
+    name: "Image compressor (2)",
+    ref: "https://compressor.io/",
   },
   {
-    name: 'SVG compressor',
-    ref: 'https://jakearchibald.github.io/svgomg/'
+    name: "SVG compressor",
+    ref: "https://jakearchibald.github.io/svgomg/",
   },
   {
-    name: 'File converter',
-    ref: 'https://file-converter-online.com/'
+    name: "File converter",
+    ref: "https://file-converter-online.com/",
   },
   {
-    name: 'PDF compressor',
-    ref: 'https://compress-pdf.file-converter-online.com/'
+    name: "PDF compressor",
+    ref: "https://compress-pdf.file-converter-online.com/",
   },
   {
-    name: 'Video compressor',
-    ref: 'https://file-converter-online.com/converter/compress-video/'
+    name: "Video compressor",
+    ref: "https://file-converter-online.com/converter/compress-video/",
   },
   {
-    name: 'Background remover (1)',
-    ref: 'https://www.remove.bg/'
+    name: "Background remover (1)",
+    ref: "https://www.remove.bg/",
   },
   {
-    name: 'Background remover (2)',
-    ref: 'https://spark.adobe.com/tools/remove-background'
+    name: "Background remover (2)",
+    ref: "https://spark.adobe.com/tools/remove-background",
   },
   {
-    name: '3D animated text generator',
-    ref: 'https://spacetypegenerator.com/'
-  }
-]
+    name: "3D animated text generator",
+    ref: "https://spacetypegenerator.com/",
+  },
+];
 
 export const galleryList = [
-  { 
-    name: 'https://softglossary.space/inspiration',
-    ref: 'https://softglossary.space'
-  },
-  { 
-    name: 'https://www.hoverstat.es/archive',
-    ref: 'https://www.hoverstat.es/archive'
+  {
+    name: "https://softglossary.space/inspiration",
+    ref: "https://softglossary.space",
   },
   {
-    name: 'https://minimal.gallery',
-    ref: 'https://minimal.gallery'
+    name: "https://www.hoverstat.es/archive",
+    ref: "https://www.hoverstat.es/archive",
   },
   {
-    name: 'https://brutalistwebsites.com',
-    ref: 'https://brutalistwebsites.com',
-    span: 'archive'
+    name: "https://minimal.gallery",
+    ref: "https://minimal.gallery",
   },
   {
-    name: 'https://cargo.site/In-Use',
-    ref: 'https://cargo.site/In-Use'
+    name: "https://brutalistwebsites.com",
+    ref: "https://brutalistwebsites.com",
+    span: "archive",
   },
   {
-    name: 'https://readymag.com/examples',
-    ref: 'https://readymag.com/examples'
+    name: "https://cargo.site/In-Use",
+    ref: "https://cargo.site/In-Use",
   },
   {
-    name: 'https://cyrillic.design',
-    ref: 'https://cyrillic.design'
+    name: "https://readymag.com/examples",
+    ref: "https://readymag.com/examples",
   },
   {
-    name: 'https://tilda.cc/ru/madeontilda/',
-    ref: 'https://tilda.cc/ru/madeontilda/'
+    name: "https://cyrillic.design",
+    ref: "https://cyrillic.design",
   },
   {
-    name: 'https://ilovecreatives.com/internet-gems',
-    ref: 'https://ilovecreatives.com/internet-gems'
+    name: "https://tilda.cc/ru/madeontilda/",
+    ref: "https://tilda.cc/ru/madeontilda/",
   },
   {
-    name: 'https://minimalonly.ru/',
-    ref: 'https://minimalonly.ru/'
+    name: "https://ilovecreatives.com/internet-gems",
+    ref: "https://ilovecreatives.com/internet-gems",
   },
   {
-    name: 'https://www.siteinspire.com/',
-    ref: 'https://www.siteinspire.com/'
+    name: "https://minimalonly.ru/",
+    ref: "https://minimalonly.ru/",
   },
   {
-    name: 'https://the-responsive.com/',
-    ref: 'https://the-responsive.com/'
+    name: "https://www.siteinspire.com/",
+    ref: "https://www.siteinspire.com/",
   },
   {
-    name: 'https://onepagelove.com/inspiration',
-    ref: 'https://onepagelove.com/inspiration'
+    name: "https://the-responsive.com/",
+    ref: "https://the-responsive.com/",
   },
   {
-    name: 'https://httpster.net/',
-    ref: 'https://httpster.net/'
+    name: "https://onepagelove.com/inspiration",
+    ref: "https://onepagelove.com/inspiration",
   },
   {
-    name: 'https://webflow.com/websites/popular',
-    ref: 'https://webflow.com/websites/popular'
+    name: "https://httpster.net/",
+    ref: "https://httpster.net/",
   },
   {
-    name: 'https://www.behance.net/galleries/ui-ux/',
-    ref: 'https://www.behance.net/galleries/ui-ux/'
+    name: "https://webflow.com/websites/popular",
+    ref: "https://webflow.com/websites/popular",
   },
   {
-    name: 'https://www.awwwards.com/websites/',
-    ref: 'https://www.awwwards.com/websites/'
+    name: "https://www.behance.net/galleries/ui-ux/",
+    ref: "https://www.behance.net/galleries/ui-ux/",
   },
   {
-    name: 'https://uijar.com/',
-    ref: 'https://uijar.com/'
-  }
-]
-
+    name: "https://www.awwwards.com/websites/",
+    ref: "https://www.awwwards.com/websites/",
+  },
+  {
+    name: "https://uijar.com/",
+    ref: "https://uijar.com/",
+  },
+];
